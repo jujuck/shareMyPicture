61fcbff889b60b3794d59d6daece4a4c8523bfe2
Julien RICHARD <julien.y.richard@gmail.com>
2024-12-19 16:40:48 +0100

    Mise en forme de la page du formulaire de partage de photos

--- client/src/pages/UploadView.vue
+++ client/src/pages/UploadView.vue
@@ -1,7 +1,36 @@
 <template lang="">
-  <div>
-    <h1>Upload</h1>
-  </div>
+  <form>
+    <div class="wedding_heading flex text-center mt-5">
+      <h1>Chargez votre image ici !!!</h1>
+      <h3>Laissez nous un petit mot également</h3>
+    </div>
+    <div class="wedding_form">
+      <label for="picture">
+        Charge ta photo
+      </label>
+      <input type="file" id="picture" required/>
+      <label for="name">
+        Ton nom et prénom
+      </label>
+      <input type="text" id="name" required/>
+      <label for="tags">
+        Tag des personnes (si tu le veux)
+      </label>
+      <input type="text" id="tags"/>
+      <label for="description">
+        Une petite description
+      </label>
+      <textarea id="description" required></textarea>
+    </div>
+    <div class="wedding_btn">
+      <button type="submit">Partager</button>
+    </div>
+  </form>
+  <section>
+    <small>
+        L'ensemble des images partagées est stocké sur notre ordinateur, dans cette même salle. Aucune image ne sera partagée en dehors de l'assemblée ici présente
+    </small>
+  </section>
 </template>
 <script>
 export default {
