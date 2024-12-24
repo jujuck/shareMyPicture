d8cedc6a25606bf65410164992f8e970d5447f7d
Julien RICHARD <julien.y.richard@gmail.com>
2024-12-24 11:14:27 +0100

    Mise en place de la récupération des champs dans le form upload

--- client/src/pages/UploadView.vue
+++ client/src/pages/UploadView.vue
@@ -1,5 +1,5 @@
 <template lang="">
-  <form>
+  <form @submit="($event) => onSubmit($event)">
     <div class="wedding_heading flex text-center mt-5">
       <h1>Chargez votre image ici !!!</h1>
       <h3>Laissez nous un petit mot également</h3>
@@ -8,19 +8,19 @@
       <label for="picture">
         Charge ta photo
       </label>
-      <input type="file" id="picture" required/>
+      <input type="file" id="picture" required @change="onFileChanged($event)"/>
       <label for="name">
         Ton nom et prénom
       </label>
-      <input type="text" id="name" required/>
+      <input type="text" id="name" required v-model="name"/>
       <label for="tags">
         Tag des personnes (si tu le veux)
       </label>
-      <input type="text" id="tags"/>
+      <input type="text" id="tags" v-model="tags"/>
       <label for="description">
         Une petite description
       </label>
-      <textarea id="description" required></textarea>
+      <textarea id="description" required v-model="description"></textarea>
     </div>
     <div class="wedding_btn">
       <button type="submit">Partager</button>
@@ -32,10 +32,27 @@
     </small>
   </section>
 </template>
-<script>
-export default {
+<script setup lang="ts">
+import { ref } from 'vue';
 
+const name = ref < string > ("");
+const tags = ref < string > ("");
+const description = ref < string > ("");
+const file = ref < File | null > ();
+
+const onFileChanged = ($event: Event) => {
+  const target = $event.target as HTMLInputElement;
+  if (target && target.files) {
+    file.value = target.files[0];
+  }
 }
+
+const onSubmit = ($event: Event) => {
+  $event.preventDefault();
+
+  console.log(name.value, description.value, file.value, tags.value)
+}
+
 </script>
 <style lang="">
 
