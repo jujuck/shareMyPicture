02227918a16687d05affd9f2ef5400059a39b08d
Julien RICHARD <julien.y.richard@gmail.com>
2025-04-28 09:18:46 +0200

    Bascule du client en pwa pour meilleure accessibilité aux systeme mobile

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,11 +1,39 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import { VitePWA } from "vite-plugin-pwa";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  server: {
-    host: "0.0.0.0",
-    port: 5173,
-  },
+  plugins: [
+    vue(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: [
+        "favicon.svg",
+        "favicon.ico",
+        "robots.txt",
+        "apple-touch-icon.png",
+      ],
+      manifest: {
+        name: "Helene et Julien",
+        short_name: "H&J",
+        description: "Partage de photos de mariage",
+        theme_color: "#ffffff",
+        background_color: "#ffffff",
+        display: "standalone",
+        start_url: "/",
+        icons: [
+          {
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
 });
